70f2347f251c9459f9c2db2c1ec841a894657d37
Seno Wijayanto <senowijayanto@gmail.com>
2023-07-26 14:17:32 +0700

    update server library

--- chapter-1.1/main.go
+++ chapter-1.1/main.go
@@ -20,9 +20,12 @@ func main() {
 	http.HandleFunc("/index", handlerIndex)
 	http.HandleFunc("/hello", handlerHello)
 
-	var address = "localhost:8000"
+	var address = ":8000"
 	fmt.Printf("Server started at %s\n", address)
-	err := http.ListenAndServe(address, nil)
+
+	server := new(http.Server)
+	server.Addr = address
+	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
